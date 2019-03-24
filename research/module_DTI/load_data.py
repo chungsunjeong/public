e34a17514fd1ce0662e7803b14a72adb5d2ba010
Chungsun Jeong <freefly7@kaist.ac.kr>
2019-03-24 23:53:59 +0900

    20190324

--- research/module_DTI/load_data.py
+++ research/module_DTI/load_data.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
-def load_general_matrix(dir,type):
-    mat = pd.read_csv(dir, delim_whitespace=True)
+def load_general_matrix(dir,type,header='infer'):
+    mat = pd.read_csv(dir, delim_whitespace=True,header=header)
     print('# of '+type+'s: ' + str(mat.shape[0]))
     print('# of features of a '+type+': ' + str(mat.shape[1]) + '\n')
     mat.index = mat.index.map(str)
     return mat
 
-def load_drug_target_interaction_adjacency_matrix(dir):
-    mat=pd.read_csv(dir, delim_whitespace=True)
+def load_drug_target_interaction_adjacency_matrix(dir,header='infer'):
+    mat=pd.read_csv(dir, delim_whitespace=True,header=header)
     print('# of drug: '+str(mat.shape[0]))
     print('# of target: '+str(mat.shape[1])+'\n')
     mat.index=mat.index.map(str)
     return mat
 
 
-def load_drug_target_interaction_dict(dir):
-    mat = pd.read_csv(dir, delim_whitespace=True, header=None,names=['target','drug'])
+def load_drug_target_interaction_dict(dir,header='infer'):
+    mat = pd.read_csv(dir, delim_whitespace=True, header=header,names=['target','drug'])
     dict_DTI=dict()
     i=0
     for value in mat.values:
@@ -31,25 +31,25 @@ def load_drug_target_interaction_dict(dir):
     return dict_DTI
 
 
-def load_drug_descriptor_matrix(dir):
-    return load_general_matrix(dir,type='drug')
+def load_drug_descriptor_matrix(dir,header='infer'):
+    return load_general_matrix(dir,type='drug',header=header)
 
 
-def load_target_descriptor_matrix(dir):
-    return load_general_matrix(dir, type='target')
+def load_target_descriptor_matrix(dir,header='infer'):
+    return load_general_matrix(dir, type='target',header=header)
 
 
-def load_drug_target_pair_matrix(dir):
-    return load_general_matrix(dir, type='drug-target pair')
+def load_drug_target_pair_matrix(dir,header='infer'):
+    return load_general_matrix(dir, type='drug-target pair',header=header)
 
 
-def load_pos_samples(dir):
-    return load_general_matrix(dir, type='positive sample')
+def load_pos_samples(dir,header='infer'):
+    return load_general_matrix(dir, type='positive sample',header=header)
 
 
-def load_neg_samples(dir):
-    return load_general_matrix(dir, type='negative sample')
+def load_neg_samples(dir,header='infer'):
+    return load_general_matrix(dir, type='negative sample',header=header)
 
 
-def load_etc(dir):
+def load_etc(dir,header='infer'):
     pass
